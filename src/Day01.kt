4d39fff4bef11887c01910913269c71f8fbeec3d
SFerjanec <stephen.ferjanec@signetjewelers.com>
2021-12-02 21:13:38 -0500

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    /* Puzzle: Figure out how quickly the depth increases
+      Count the number of times a depth measurement increases from
+     the previous measurement.
+     Compare each number to the previous one. Compare
+     Compare the second to the first, the third to the second
+     the fourth to the third...
+    */
+
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a,b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // A + B + C <=> B + C + D
+    fun part2(input: List<Int>): Int {
+// solution 1:    return input.windowed(3).windowed(2).count {
+//                (a,b) -> a.sum() < b.sum()
+// solution 2: subtract B + C from both sides of the equation.
+        // now you can compare A and D.
+        return input
+            .windowed(4)
+            .count { it[0] < it[3]
+
+    }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
 }
